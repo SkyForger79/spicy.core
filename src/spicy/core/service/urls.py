6d92a561d3fcd6483e92c239e2c465c39810793c
Stas Shtin <antisvin@gmail.com>
2013-04-17 00:38:16 +0400

    #444

--- src/spicy/core/service/urls.py
+++ src/spicy/core/service/urls.py
@@ -9,13 +9,17 @@ admin_urls = patterns(
     'spicy.core.service.admin',
     url(r'^$', 'index', name='index'),
     url(r'^services/$', 'services', name='services'),
-)    
+)
 
 admin_urls += api.register.urls()
 public_urls += api.register.urls(is_public=True)
 
 urlpatterns = patterns('',)
 if defaults.USE_CUSTOM_ADMIN:
-    urlpatterns += patterns('', url(r'^admin/', include(admin_urls, namespace='admin')),)
+    urlpatterns += patterns(
+        '',
+        url(r'^admin/', include(admin_urls, namespace='admin')),)
 
-urlpatterns += patterns('', url(r'^', include(public_urls, namespace='public')),)
+urlpatterns += patterns(
+    '',
+    url(r'^', include(public_urls, namespace='public')),)
--- src/spicy/core/siteskin/urls.py
+++ src/spicy/core/siteskin/urls.py
@@ -20,17 +20,6 @@ public_urls += patterns(
 
 admin_urls = patterns(
     'spicy.core.siteskin.admin',
-    url(r'^list/$', 'block_list', name='index'),
-    url(r'^create/(?P<content_service_name>[-\w]+)/$', 'create',
-        name='create'),
-    url(r'^edit/(?P<block_id>[-\d]+)/$', 'edit', name='edit'),
-    url(r'^edit/(?P<slug>[-\w_]+)/$', 'edit_by_slug', name='edit_by_slug'),
-    url(r'^delete/(?P<block_id>[-\d]+)/$', 'delete', name='delete'),
-    url(r'^delete/block-list/$', 'delete_block_list',
-        name='delete-block-list'),
-    url(r'^deleted/$', 'deleted', name='deleted-in-dialog'),
-    url(r'^is-deleted/$', 'is_deleted', name='is-deleted'),
-    url(r'^create_rss/$', 'create_rss', name='create_rss')
     )
 
 

b37af941fe864a61eb632a34b1a640124e86207b
Stas Shtin <antisvin@gmail.com>
2013-07-09 20:12:47 +0000

    Search TEMPLATE_DIRS

--- src/spicy/utils/templates.py
+++ src/spicy/utils/templates.py
@@ -1,23 +1,25 @@
 import os
+from django.conf import settings
 from spicy.core.admin.conf import app_modules_register
 
 
 def find_templates(base_dir, name_tuples=True):
     templates = []
-    for app in app_modules_register.values():
+    paths = [
+        os.path.join(
+            os.path.dirname(app.__file__), 'templates',
+            base_dir)
+        for app in app_modules_register.values()]
+    paths.extend(settings.TEMPLATE_DIRS)
+    for path in paths:
         try:
             if name_tuples:
                 templates.extend(
-                    [(tmpl, tmpl) for tmpl in os.listdir(
-                        os.path.join(
-                            os.path.dirname(app.__file__), 'templates',
-                            base_dir))])
+                    [(tmpl, tmpl) for tmpl in os.listdir(path)])
             else:
-                dir_name = os.path.join(
-                    os.path.dirname(app.__file__), 'templates', base_dir)
                 templates.extend(
-                    [os.path.join(dir_name, template)
-                     for template in os.listdir(dir_name)])
+                    [os.path.join(path, template)
+                     for template in os.listdir(path)])
         except OSError:
             pass
     templates.sort()

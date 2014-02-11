e444c3f166e0fa87c6fb434aad2f35f382339dce
Stas Shtin <antisvin@gmail.com>
2014-02-11 19:25:10 +0400

    Locale

--- src/spicy/core/simplepages/forms.py
+++ src/spicy/core/simplepages/forms.py
@@ -4,7 +4,14 @@ from . import defaults
 
 
 class SimplePageForm(forms.ModelForm):
+    def clean_url(self):
+        value = self.cleaned_data['url']
+        if value and not value.startswith('/'):
+            value = '/' + value
+        return value
+
     class Meta:
         model = utils.get_custom_model_class(defaults.SIMPLE_PAGE_MODEL)
-        fields = ('title', 'url', 'content', 'enable_comments',
+        fields = (
+            'title', 'url', 'content', 'enable_comments',
             'registration_required', 'sites', 'template_name')

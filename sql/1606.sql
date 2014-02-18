4e443218971fefab8468d59de77374eb9b766ac8
Alexandr Zolotarev <megabogus84@gmail.com>
2014-02-18 09:29:34 +0400

    remove sql 1606

--- sql/1606.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-set constraints all immediate;
-ALTER TABLE sp_simplepage ADD COLUMN og_title varchar(255);
-ALTER TABLE sp_simplepage ADD COLUMN og_description varchar(255);
-ALTER TABLE sp_simplepage ADD COLUMN og_url varchar(255);
-ALTER TABLE sp_simplepage ADD COLUMN og_image varchar(255);
-COMMIT;

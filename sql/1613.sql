9c889e7307409b4c77b44a97ea0265813a79bdaa
Alexandr Zolotarev <megabogus84@gmail.com>
2014-02-20 14:04:47 +0400

    #1613 ?????? ? sql

--- sql/1613.sql
+++ sql/1613.sql
@@ -1,9 +1,10 @@
 begin;
 set constraints all immediate;
 alter table spicy_settings add column robots text;
-insert into spicy_settings values (2,'','','','','f','f','','',3,(select robots from seo_chunks where site_id = 3));
-insert into spicy_settings values (3,'','','','','f','f','','',2,(select robots from seo_chunks where site_id = 2));
-insert into spicy_settings values (4,'','','','','f','f','','',18,(select robots from seo_chunks where site_id = 18));
+insert into spicy_settings (license_pub_key,sentry_key,redmine_key,redmine_project,enable_debug_toolbar,debug_mode,admins_emails,managers_emails,site_id,robots) values ('','','','','f','f','','',1,(select robots  from seo_chunks where site_id = 1));
+insert into spicy_settings (license_pub_key,sentry_key,redmine_key,redmine_project,enable_debug_toolbar,debug_mode,admins_emails,managers_emails,site_id,robots) values ('','','','','f','f','','',3,(select robots from seo_chunks where site_id = 3));
+insert into spicy_settings (license_pub_key,sentry_key,redmine_key,redmine_project,enable_debug_toolbar,debug_mode,admins_emails,managers_emails,site_id,robots) values ('','','','','f','f','','',2,(select robots from seo_chunks where site_id = 2));
+insert into spicy_settings (license_pub_key,sentry_key,redmine_key,redmine_project,enable_debug_toolbar,debug_mode,admins_emails,managers_emails,site_id,robots) values ('','','','','f','f','','',18,(select robots from seo_chunks where site_id = 18));
 UPDATE spicy_settings SET robots = (select robots from seo_chunks where site_id = 1) where site_id = 1;
 commit;
 begin;

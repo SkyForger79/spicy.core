8d837e7b49f01a382cf8369b14d8574ded8e9ec7
Asta Kovrova <ksawie@gmail.com>
2014-09-26 13:05:27 +0400

    #2660 added full path with port to admin context processor

--- src/spicy/core/admin/context_processors.py
+++ src/spicy/core/admin/context_processors.py
@@ -2,6 +2,14 @@ from . import defaults
 from .conf import admin_apps_register
 
 
+def path_with_port(request):
+
+    if request.META['SERVER_PORT']:
+        return 'http://' + request.get_host()+ ':' + request.META['SERVER_PORT']
+    else:
+        return 'http://' + request.get_host()
+
+
 def base(request):
     return {
         'ADMIN_APPS': sorted(
@@ -9,5 +17,6 @@ def base(request):
             key=lambda x: x.order_number, reverse=False),
         'ADMIN_APPS_REGISTER': admin_apps_register,
         'ADMIN_DASHBOARD_APPS': defaults.ADMIN_DASHBOARD_APPS,
-        }
+        'FULL_PATH_WITH_PORT': path_with_port(request)
 
+    }
